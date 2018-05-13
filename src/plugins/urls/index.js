5294386ef264d20462d78707d97d5a483832ef32
homer0 <leo.apiwan@olapic.com>
2018-05-13 04:57:02 -0300

    Add the URLs plugin

---
+++ src/plugins/urls/index.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const rollupUtils = require('rollup-pluginutils');
+const extend = require('extend');
+const fs = require('fs-extra');
+
+class RollupURLsPlugin {
+  constructor(options = {}, name) {
+    this.options = extend(
+      true,
+      {
+        urls: [],
+      },
+      options
+    );
+
+    this.name = name || 'rollup-plugin-urls';
+
+    if (!this.options.urls.length) {
+      throw new Error(`${this.name}: You need define the urls`);
+    }
+
+    this.options.urls = this.options.url.map((urlSettings) => Object.assign(
+      urlSettings,
+      {
+        filter: rollupUtils.createFilter(
+          urlSettings.include,
+          urlSettings.exclude
+        ),
+      }
+    ));
+
+    this._toCopy = [];
+    this.load = this.load.bind(this);
+    this.ongenerate = this.ongenerate.bind(this);
+  }
+
+  load(filepath) {
+    let result = null;
+    const settings = this.options.urls.find((urlSettings) => urlSettings.filter(filepath));
+    if (settings) {
+      const info = path.parse(filepath);
+      this._toCopy.push({
+        from: filepath,
+        to: this._formatName(settings.output, info),
+      });
+
+      const fileURL = this._formatName(settings.url, info);
+      result = `export default '${fileURL}';`;
+    }
+
+    return result;
+  }
+
+  ongenerate() {
+    this._toCopy.forEach((toCopy) => {
+      fs.ensureDirSync(path.dirname(toCopy.to));
+      fs.copySync(toCopy.from, toCopy.to);
+    });
+  }
+
+  _formatName(placeholder, info) {
+    return placeholder
+    .replace(/\[name\]/g, info.name)
+    .replace(/\[ext\]/g, info.ext.substr(1));
+  }
+}
+
+const urls = (options, name) => new RollupURLsPlugin(options, name);
+
+module.exports = {
+  RollupURLsPlugin,
+  urls,
+};
