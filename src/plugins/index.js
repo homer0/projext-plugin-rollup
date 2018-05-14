8df2ca6ae4a97570da8751d62f22ca70734c0951
homer0 <leo.apiwan@olapic.com>
2018-05-14 06:08:09 -0300

    Update plugins index

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,11 @@
 const { css } = require('./css');
+const { stylesheetAssets } = require('./stylesheetAssets');
+const { template } = require('./template');
+const { urls } = require('./urls');
 
 module.exports = {
   css,
+  stylesheetAssets,
+  template,
+  urls,
 };
