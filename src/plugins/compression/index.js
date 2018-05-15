6809bea019f9a57aa9448774fe8bcee5fc39f74c
homer0 <leo.apiwan@olapic.com>
2018-05-15 07:53:02 -0300

    Add the compression plugin

---
+++ src/plugins/compression/index.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const rollupUtils = require('rollup-pluginutils');
+const extend = require('extend');
+const fs = require('fs-extra');
+const zopfli = require('node-zopfli');
+
+class RollupCompressionPlugin {
+  constructor(options = {}, name) {
+    this.options = extend(
+      true,
+      {
+        folder: './dist',
+        include: [],
+        exclude: [],
+      },
+      options
+    );
+
+    this.name = name || 'rollup-plugin-compression';
+    this.options.include = this.options.include || [/\.css$/i];
+    this.filter = rollupUtils.createFilter(
+      this.options.include,
+      this.options.exclude
+    );
+
+    this.ongenerate = this.ongenerate.bind(this);
+  }
+
+  ongenerate() {
+    return this._findAllTheFiles()
+    .then((files) => Promise.all(files.map((file) => this._compressFile(file))))
+    .catch((error) => {
+      throw error;
+    });
+  }
+
+  _findAllTheFiles() {
+    return this._readDirectory(this.options.folder);
+  }
+
+  _readDirectory(directory) {
+    const result = [];
+    fs.readdirSync(directory)
+    .filter((item) => !item.startsWith('.') && !item.endsWith('.gz'))
+    .map((item) => path.join(directory, item))
+    .filter((item) => this.filter(item))
+    .forEach((item) => {
+      if (fs.lstatSync(item).isDirectory()) {
+        result.push(...this._readDirectory(item));
+      } else {
+        result.push(item);
+      }
+    });
+
+    return result;
+  }
+
+  _compressFile(filepath) {
+    return new Promise((resolve, reject) => {
+      const newFilepath = `${filepath}.gz`;
+      fs.createReadStream(filepath)
+      .on('error', reject)
+      .pipe(zopfli.createGzip())
+      .pipe(fs.createWriteStream(newFilepath))
+      .on('error', reject)
+      .on('close', () => resolve({
+        original: filepath,
+        compressed: newFilepath,
+      }));
+    });
+  }
+}
+
+const compression = (options, name) => new RollupCompressionPlugin(options, name);
+
+module.exports = {
+  RollupCompressionPlugin,
+  compression,
+};
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,3 +1,4 @@
+const { compression } = require('./compression');
 const { css } = require('./css');
 const { devServer } = require('./devServer');
 const { stylesheetAssets } = require('./stylesheetAssets');
@@ -5,6 +6,7 @@ const { template } = require('./template');
 const { urls } = require('./urls');
 
 module.exports = {
+  compression,
   css,
   devServer,
   stylesheetAssets,
