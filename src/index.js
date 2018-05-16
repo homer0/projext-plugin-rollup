db8fb0affd9a55ca7fec6d4afded3b38c3e3f11a
homer0 <leo.apiwan@olapic.com>
2018-05-16 02:29:10 -0300

    Register services

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const { name } = require('../package.json');
 
+const {
+  rollupConfiguration,
+  rollupBuildEngine,
+} = require('./services/building');
+
+const {
+  rollupBrowserDevelopmentConfiguration,
+  rollupBrowserProductionConfiguration,
+  rollupFileRulesConfiguration,
+  rollupNodeDevelopmentConfiguration,
+  rollupNodeProductionConfiguration,
+  rollupPluginSettingsConfiguration,
+} = require('./services/configurations');
+
 const loadPlugin = (app) => {
   app.set('rollupPluginInfo', () => ({
     name,
@@ -9,6 +23,16 @@ const loadPlugin = (app) => {
       'jimpex',
     ],
   }));
+
+  app.register(rollupConfiguration);
+  app.register(rollupBuildEngine);
+
+  app.register(rollupBrowserDevelopmentConfiguration);
+  app.register(rollupBrowserProductionConfiguration);
+  app.register(rollupFileRulesConfiguration);
+  app.register(rollupNodeDevelopmentConfiguration);
+  app.register(rollupNodeProductionConfiguration);
+  app.register(rollupPluginSettingsConfiguration);
 };
 
 module.exports = loadPlugin;
